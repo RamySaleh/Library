d64c861c47b58c983a650d992dd55a6ac8f3fa71
RamySaleh <ramy.saleh.me@gmail.com>
2017-06-23 14:13:16 +0200

    removed ui first first commit

---
+++ Library.BAL/BookBAL.cs
@@ -0,0 +1,23 @@
+﻿using Library.DAL;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.BAL
+{
+    public class BookBAL
+    {
+        string connectionString;
+        public BookBAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public List<Book> GetAllBooks()
+        {
+            return new BookRepo(connectionString).GetAll();
+        }
+    }
+}
