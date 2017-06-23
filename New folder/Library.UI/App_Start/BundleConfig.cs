e55ad46d796d2a2a16de7cdfc0e9c1c69b9dc135
RamySaleh <ramy.saleh.me@gmail.com>
2017-06-23 15:24:03 +0200

    added mvc client

--- New folder/Library.UI/App_Start/BundleConfig.cs
+++ New folder/Library.UI/App_Start/BundleConfig.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Optimization;
 
 namespace Library.UI
 {
     public class BundleConfig
     {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
