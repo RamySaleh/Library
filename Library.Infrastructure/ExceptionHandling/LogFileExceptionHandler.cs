73ecac14a39abd00c4bebca2534301dc88384127
RamySaleh <ramy.saleh.me@gmail.com>
2017-06-24 14:40:00 +0200

    added exception handling

---
+++ Library.Infrastructure/ExceptionHandling/LogFileExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infrastructure.ExceptionHandling
+{
+    public class LogFileExceptionHandler : IExceptionHandler
+    {
+        public const string logFilePath = "log.txt";
+        public void HandleException(Exception ex)
+        {
+            var exceptionLog = new List<string>();
+            exceptionLog.Add( $"Time : {DateTime.Now.ToLocalTime().ToString()}, Message : {ex.Message}");
+            exceptionLog.Add( $"Stack Trace : {ex.StackTrace}");
+            exceptionLog.Add( $"--------------------------------");
+
+            File.AppendAllLines(logFilePath, exceptionLog);
+        }
+    }
+}
