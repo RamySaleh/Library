dc2d8816d26c63a2a33140c5977b5c614bd48ec2
RamySaleh <ramy.saleh.me@gmail.com>
2017-07-23 11:30:29 +0200

    added specflow

---
+++ Library.Specs/UserRegisteration.feature
@@ -0,0 +1,10 @@
+﻿Feature: UserRegisteration
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Register new user
+	Given I am on the registeration page
+	When I enter my data
+	And Proceed with user creation
+	Then The user is created and redirected to the correct page
---
+++ Library.Specs/UserRegisterationSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Library.Specs
+{
+    [Binding]
+    public class UserRegisterationSteps
+    {
+        [Given]
+        public void GivenIAmOnTheRegisterationPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When]
+        public void WhenIEnterMyData()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When]
+        public void WhenProceedWithUserCreation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then]
+        public void ThenTheUserIsCreatedAndRedirectedToTheCorrectPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
